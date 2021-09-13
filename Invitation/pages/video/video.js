b45b3ff17e42bff0da6aeb6985cf83a28a3a14ae
shanlei <15308255956@163.com>
2021-09-13 21:04:09 +0800

    腾讯视频插件实现播放视频

--- Invitation/pages/video/video.js
+++ Invitation/pages/video/video.js
@@ -5,7 +5,8 @@ Page({
      * 页面的初始数据
      */
     data: {
-        src:"https://cn-scya-dx-bcache-06.bilivideo.com/upgcxcode/49/64/174636449/174636449_nb2-1-16.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1631541516&gen=playurlv2&os=bcache&oi=3063197622&trid=0000a1debcbc18cf4169be150ad626fee1f4h&platform=html5&upsig=036f22784a5536d10f99009420fc70b5&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=62706&mid=106205903&bvc=vod&nettype=0&logo=80000000",
+        // src:"https://cn-scya-dx-bcache-06.bilivideo.com/upgcxcode/49/64/174636449/174636449_nb2-1-16.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1631541516&gen=playurlv2&os=bcache&oi=3063197622&trid=0000a1debcbc18cf4169be150ad626fee1f4h&platform=html5&upsig=036f22784a5536d10f99009420fc70b5&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&cdnid=62706&mid=106205903&bvc=vod&nettype=0&logo=80000000",
+        src:"q3148q0gpou",
         danmuList:[
             {text:"好甜", color:"#ff0000", time:1},
             {text:"新婚快乐", color:"#ff00ff", time:3}
